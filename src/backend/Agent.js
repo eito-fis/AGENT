d09fd49ddbeab50d5c0e4997c07bc6215d92f0f1
Eamon Ito-Fisher <eamon@itofisher.com>
2021-02-12 20:54:08 -0800

    ADD: Abstract Agent class done

---
+++ src/backend/Agent.js
@@ -0,0 +1,54 @@
+class Agent {
+	/**
+	 * Agent Class
+	 */
+
+	constructor(env, trainSteps, loggingPeriod) {
+		this.env = env;
+		this.model = this.buildModel();
+		this.trainSteps = trainSteps;
+		this.loggingPeriod = loggingPeriod;
+		this.loggedStates = [];
+		this.metrics = {
+			"Losses": [],
+			"Average Reward": [],
+		}
+	}
+
+	// Initialize model
+	buildModel() {}
+
+	// Take in an observation, return an action
+	policy() {}
+
+	// Take in number of episodes to train, update model
+	// References trainSteps and loggingPeriod
+	train() {}
+
+	// Takes `steps` number of steps and returns results
+	rollout(steps, episodic=false) {
+		states = [];
+		actions = [];
+		rewards = [];
+		dones = [];
+		state = env.reset();
+		states.push(state);
+		for (i = 0; i < steps; i++) {
+			action = this.policy(state);
+			state, reward, done = this.env.step(action);
+			states.push(state);
+			actions.push(action);
+			rewards.push(reward);
+			dones.push(done);
+			if (done) {
+				if (episodic) {
+					break;
+				} else {
+					state = env.reset();
+				}
+			}
+		}
+		states = states.pop();
+		return states, actions, rewards, dones
+	}
+}
