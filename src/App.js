924d3ae65fe79c3b898374a7ae4d17c37d36b6dc
Nom Phan <np656@drexel.edu>
2021-02-12 23:29:48 -0500

    Welcome page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import React from 'react';
+import Home from './Home';
+import EnvAgentTab from './components/EnvAgentTab'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/envagent' exact component={EnvAgentTab} />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
