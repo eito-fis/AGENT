d9c268de122e5e1aa16124a97c6964c2c67cb9f5
William Ocampo <55007870+Polvyer@users.noreply.github.com>
2021-02-14 01:44:12 -0600

    "Added onClick for run tab"

--- src/components/Environment.js
+++ src/components/Environment.js
@@ -1,29 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
 import styled from 'styled-components';
-import { SharingContext } from '../context/SharingContext';
 
 const Container = styled.div`
   flex: 1;
 `;
 
-const Environment = ({divRef}) => {
-
-  const canvasRef = useRef(null);
-  const [ renderingContext, setRenderingContext ] = useState(null);
-  
-  useEffect(() => {
-    const context2d = canvasRef.current.getContext("2d");
-    setRenderingContext(context2d);
-  }, []);
-
+const Environment = ({ divRef, canvasRef }) => {
 
   return (
-    <SharingContext.Provider value={renderingContext}>
+    <>
       <Container>
 				<div ref={divRef}></div>
         <canvas ref={canvasRef} />
       </Container>
-    </SharingContext.Provider>
+    </>
   );
 };
 
--- src/components/Tabs/VisualTab.js
+++ src/components/Tabs/VisualTab.js
@@ -1,13 +1,60 @@
-import React from 'react';
+import React, { useContext, useRef, useState, useEffect } from 'react';
 import Environment from '../Environment';
-
+import Dropdown from '../DropDown/Dropdown';
+import { usePopulate } from '../../hooks/usePopulate';
+import { CurrentState } from "../../context/CurrentState";
+import DropdownContainer from '../DropDown/DropdownContainer';
+import { handleDropdownHeaderClick } from '../../helper/handleDropdownHeaderClick';
+import { SharingContext } from '../../context/SharingContext';
 
 const VisualTab = () => {
 
+  const canvasRef = useRef(null);
+  const [ renderingContext, setRenderingContext ] = useState(null);
+
+  // Get canvas context
+  useEffect(() => {
+    const context2d = canvasRef.current.getContext("2d");
+    setRenderingContext(context2d);
+  }, []);
+
+  const { envs, agents, setAgents } = useContext(CurrentState);
+
+  let dropdownContentArr = [
+    {
+      header: 'Controls',
+      sections: ['Run', 'Reset'],
+    }
+  ];
+
+  const [ dropdownVisibleArr, setDropdownVisibleArr ] = usePopulate(dropdownContentArr, true, 'visible');
+
+  // When user clicks on dropdown section => TO-DO
+  const handleDropdownSectionClick = (item, e) => {
+    const length = agents.loggedStates.length;
+    console.log('length:', length)
+    let states = agents.loggedStates[length - 1];
+    console.log('States:', states)
+    states.forEach((state) => {
+      console.log('State:', state)
+      envs.render(state, renderingContext);
+    });
+  };
+
   return (
-    <>
-      <Environment />
-    </>
+
+    <SharingContext.Provider value={renderingContext}>
+      <DropdownContainer>
+        {dropdownContentArr.map((dropdownContent, index) => 
+          <Dropdown 
+            key={index} 
+            contentObj={dropdownContent} 
+            index={index} 
+            headerCallback={handleDropdownHeaderClick.bind(null, dropdownVisibleArr, setDropdownVisibleArr)} 
+            sectionCallback={handleDropdownSectionClick} />)}
+      </DropdownContainer>
+      <Environment canvasRef={canvasRef} />
+    </SharingContext.Provider>
   );
 };
 
